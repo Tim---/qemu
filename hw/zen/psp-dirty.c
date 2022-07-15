a8567e4bbba763f12a1ef3f65d7ef4f937dcb2df
Timothée COCAULT <timothee.cocault@gmail.com>
2022-07-15 14:45:30 +0200

    Change "config" space to RAM

--- hw/zen/psp-dirty.c
+++ hw/zen/psp-dirty.c
@@ -7,67 +7,6 @@
 #include "hw/zen/psp-fuses.h"
 
 
-/* Config */
-
-/*
-typedef struct {
-    uint8_t PhysDieId;
-    uint8_t SocketId;
-    uint8_t PackageType;
-    uint8_t SystemSocketCount;
-    uint8_t unknown0;
-    uint8_t DiesPerSocket;
-} PspBootRomServices_t;
-*/
-static uint64_t config_read(void *opaque, hwaddr addr, unsigned size)
-{
-    switch(addr) {
-    case 3:
-    case 5:
-        return 1;
-    }
-    qemu_log_mask(LOG_UNIMP, "%s: unimplemented device read  "
-                  "(offset 0x%lx, size 0x%x)\n", __func__, addr, size);
-    return 0;
-}
-
-static void config_write(void *opaque, hwaddr addr,
-                                uint64_t value, unsigned size)
-{
-    qemu_log_mask(LOG_UNIMP, "%s: unimplemented device write "
-                  "(offset 0x%lx, size 0x%x, value 0x%lx)\n",
-                  __func__, addr, size, value);
-}
-
-static const MemoryRegionOps config_ops = {
-    .read = config_read,
-    .write = config_write,
-    .endianness = DEVICE_NATIVE_ENDIAN,
-};
-
-static void create_config(MemoryRegion *container, uint32_t addr)
-{
-    MemoryRegion *mr = g_malloc0(sizeof(MemoryRegion));
-    memory_region_init_io(mr, NULL, &config_ops, NULL, "config", 0x6);
-    memory_region_add_subregion(container, addr, mr);
-}
-
-void psp_create_config(MemoryRegion *container, zen_codename codename)
-{
-    switch(codename) {
-    case CODENAME_SUMMIT_RIDGE:
-    case CODENAME_PINNACLE_RIDGE:
-        create_config(container, 0x3fa50);
-        break;
-    case CODENAME_MATISSE:
-    case CODENAME_VERMEER:
-        create_config(container, 0x4fd50);
-        break;
-    default:
-        break;
-    }
-}
-
 /* Fuses */
 
 void psp_dirty_fuses(zen_codename codename, DeviceState *dev)
--- include/hw/zen/psp-dirty.h
+++ include/hw/zen/psp-dirty.h
@@ -1,7 +1,6 @@
 #ifndef PSP_DIRTY_H
 #define PSP_DIRTY_H
 
-void psp_create_config(MemoryRegion *container, zen_codename codename);
 void psp_dirty_fuses(zen_codename codename, DeviceState *dev);
 
 #endif
