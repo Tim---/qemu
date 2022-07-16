8b050d94bc8ea68fa2c8535823f3ceae6d099692
Timothée COCAULT <timothee.cocault@gmail.com>
2022-07-16 21:15:12 +0200

    Add Zen-based PC

---
+++ hw/i386/pc_zen.c
@@ -0,0 +1,108 @@
+#include "qemu/osdep.h"
+#include "hw/i386/x86.h"
+#include "cpu.h"
+#include "hw/zen/zen-rom.h"
+#include "hw/zen/zen-cpuid.h"
+#include "sysemu/block-backend.h"
+#include "exec/address-spaces.h"
+#include "sysemu/reset.h"
+
+struct PcZenMachineClass {
+    X86MachineClass parent;
+};
+
+struct PcZenMachineState {
+    X86MachineState parent;
+    uint32_t seg_base;
+};
+
+#define TYPE_PC_ZEN_MACHINE   MACHINE_TYPE_NAME("pc-zen")
+OBJECT_DECLARE_TYPE(PcZenMachineState, PcZenMachineClass, PC_ZEN_MACHINE)
+
+static void pc_zen_simulate_psp_boot(PcZenMachineState *mms)
+{
+    // Simulate PSP bootloader
+    DriveInfo *dinfo = drive_get(IF_MTD, 0, 0);
+    assert(dinfo);
+    BlockBackend *blk = blk_by_legacy_dinfo(dinfo);
+    assert(blk);
+
+    zen_rom_infos_t infos;
+    assert(zen_rom_init(&infos, blk, CODENAME_MATISSE));
+
+    uint32_t bin_offset;
+    uint32_t bin_size;
+    uint32_t bin_dest;
+    assert(zen_rom_get_bios_entry(&infos, AMD_BIOS_BIN, &bin_offset, &bin_size, &bin_dest));
+
+    g_autofree void *buf = g_malloc(bin_size);
+    zen_rom_read(&infos, bin_offset, buf, bin_size);
+    address_space_write(&address_space_memory, bin_dest, MEMTXATTRS_UNSPECIFIED, buf, bin_size);
+
+    mms->seg_base = bin_dest + bin_size - 0x10000;
+
+    // TODO: temporary
+    monitor_remove_blk(blk);
+}
+
+static void pc_zen_machine_reset(MachineState *machine)
+{
+    PcZenMachineState *mms = PC_ZEN_MACHINE(machine);
+    CPUState *cs;
+
+    qemu_devices_reset();
+
+    CPU_FOREACH(cs) {
+        X86CPU *cpu = X86_CPU(cs);
+        CPUX86State *env = &cpu->env;
+        cpu_reset(cs);
+        cpu_x86_load_seg_cache(env, R_CS, 0xf000, mms->seg_base, 0xffff,
+                            DESC_P_MASK | DESC_S_MASK | DESC_CS_MASK |
+                            DESC_R_MASK | DESC_A_MASK);
+    }
+}
+
+static void pc_zen_machine_state_init(MachineState *machine)
+{
+    X86MachineState *x86ms = X86_MACHINE(machine);
+    PcZenMachineState *mms = PC_ZEN_MACHINE(machine);
+
+    x86_cpus_init(x86ms, CPU_VERSION_LATEST);
+
+    memory_region_add_subregion(get_system_memory(), 0, machine->ram);
+
+    pc_zen_simulate_psp_boot(mms);
+}
+
+static void pc_zen_machine_initfn(Object *obj)
+{
+}
+
+static void pc_zen_class_init(ObjectClass *oc, void *data)
+{
+    MachineClass *mc = MACHINE_CLASS(oc);
+
+    mc->init = pc_zen_machine_state_init;
+
+    mc->family = "pc_zen";
+    mc->desc = "AMD PC with Zen architecture";
+    mc->default_cpu_type = TARGET_DEFAULT_CPU_TYPE;
+    mc->default_ram_id = "pc_zen.ram";
+    mc->max_cpus = 1;
+    mc->reset = pc_zen_machine_reset;
+}
+
+static const TypeInfo pc_zen_machine_info = {
+    .name          = TYPE_PC_ZEN_MACHINE,
+    .parent        = TYPE_X86_MACHINE,
+    .instance_size = sizeof(PcZenMachineState),
+    .instance_init = pc_zen_machine_initfn,
+    .class_size    = sizeof(PcZenMachineClass),
+    .class_init    = pc_zen_class_init,
+};
+
+static void pc_zen_machine_init(void)
+{
+    type_register_static(&pc_zen_machine_info);
+}
+type_init(pc_zen_machine_init);
