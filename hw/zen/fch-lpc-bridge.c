2045c5fc57b4b1479faebf48558bab4c0fca894e
Timothée COCAULT <timothee.cocault@gmail.com>
2022-07-22 23:22:31 +0200

    Change FCH LPC bridge to PCI device

--- hw/zen/fch-lpc-bridge.c
+++ hw/zen/fch-lpc-bridge.c
@@ -1,5 +1,5 @@
 #include "qemu/osdep.h"
-#include "hw/sysbus.h"
+#include "hw/pci/pci.h"
 #include "qemu/log.h"
 #include "hw/zen/fch-lpc-bridge.h"
 
@@ -7,64 +7,65 @@ OBJECT_DECLARE_SIMPLE_TYPE(FchLpcBridgeState, FCH_LPC_BRIDGE)
 
 struct FchLpcBridgeState {
     /*< private >*/
-    SysBusDevice parent_obj;
+    PCIDevice parent_obj;
 
     /*< public >*/
     MemoryRegion regs_region;
 };
 
 
-static uint64_t fch_lpc_bridge_io_read(void *opaque, hwaddr addr, unsigned size)
+static uint32_t fch_lpc_bridge_config_read(PCIDevice *d, uint32_t addr, int size)
 {
     qemu_log_mask(LOG_UNIMP, "%s: unimplemented device read  "
-                "(offset 0x%lx, size 0x%x)\n", __func__, addr, size);
+                "(offset 0x%x, size 0x%x)\n", __func__, addr, size);
     return 0;
 }
 
-static void fch_lpc_bridge_io_write(void *opaque, hwaddr addr,
-                                    uint64_t value, unsigned size)
+static void fch_lpc_bridge_config_write(PCIDevice *d, uint32_t addr,
+                                    uint32_t value, int size)
 {
     qemu_log_mask(LOG_UNIMP, "%s: unimplemented device write  "
-                "(offset 0x%lx, size 0x%x, value 0x%lx)\n", __func__, addr, size, value);
+                "(offset 0x%x, size 0x%x, value 0x%x)\n", __func__, addr, size, value);
 
 }
-
-static const MemoryRegionOps fch_lpc_bridge_io_ops = {
-    .read = fch_lpc_bridge_io_read,
-    .write = fch_lpc_bridge_io_write,
-    .endianness = DEVICE_NATIVE_ENDIAN,
-};
 
 static void fch_lpc_bridge_init(Object *obj)
 {
 }
 
-static void fch_lpc_bridge_realize(DeviceState *dev, Error **errp)
+static void fch_lpc_bridge_realize(PCIDevice *dev, Error **errp)
 {
-    SysBusDevice *sbd = SYS_BUS_DEVICE(dev);
-    FchLpcBridgeState *s = FCH_LPC_BRIDGE(dev);
+}
 
-    /* Init the registers access */
-    memory_region_init_io(&s->regs_region, OBJECT(s), &fch_lpc_bridge_io_ops, s,
-                          "fch-lpc-bridge-regs",
-                          0x1000);
-    sysbus_init_mmio(sbd, &s->regs_region);
+static void fch_lpc_bridge_exit(PCIDevice *dev)
+{
 }
 
 static void fch_lpc_bridge_class_init(ObjectClass *oc, void *data)
 {
     DeviceClass *dc = DEVICE_CLASS(oc);
+    PCIDeviceClass *k = PCI_DEVICE_CLASS(oc);
 
-    dc->realize = fch_lpc_bridge_realize;
     dc->desc = "FCH LPC bridge";
+    k->realize      = fch_lpc_bridge_realize;
+    k->exit         = fch_lpc_bridge_exit;
+    k->vendor_id    = PCI_VENDOR_ID_AMD;
+    k->device_id    = 0x790E;
+    k->class_id     = PCI_CLASS_BRIDGE_ISA;
+    k->config_write = fch_lpc_bridge_config_write;
+    k->config_read  = fch_lpc_bridge_config_read;
 }
 
 static const TypeInfo fch_lpc_bridge_type_info = {
     .name = TYPE_FCH_LPC_BRIDGE,
-    .parent = TYPE_SYS_BUS_DEVICE,
+    .parent = TYPE_PCI_DEVICE,
     .instance_size = sizeof(FchLpcBridgeState),
     .instance_init = fch_lpc_bridge_init,
     .class_init = fch_lpc_bridge_class_init,
+    .interfaces = (InterfaceInfo[]) {
+        { INTERFACE_CONVENTIONAL_PCI_DEVICE },
+        { }
+    },
 };
 
 static void fch_lpc_bridge_register_types(void)
