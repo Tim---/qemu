3536d78013a787580b2662d2c11f1392bee8e2b1
Timothée COCAULT <timothee.cocault@gmail.com>
2022-07-26 12:08:34 +0200

    OCBL: add public key

--- hw/zen/psp-ocbl.c
+++ hw/zen/psp-ocbl.c
@@ -42,33 +42,46 @@ static void psp_create_config(AddressSpace *as, zen_codename codename)
     }
 }
 
-static void copy_psp_dir(AddressSpace *as, zen_rom_infos_t *infos, zen_codename codename)
+static uint32_t get_psp_dir_addr(zen_codename codename)
 {
-    uint32_t psp_dir_addr;
-    psp_directory_table *psp_dir = infos->psp_dir;
-    size_t psp_dir_size = sizeof(psp_directory_header) +
-                          sizeof(psp_directory_entry) * psp_dir->header.num_entries;
-
-
     switch(codename) {
     case CODENAME_SUMMIT_RIDGE:
     case CODENAME_PINNACLE_RIDGE:
     case CODENAME_RAVEN_RIDGE:
     case CODENAME_PICASSO:
-        psp_dir_addr = 0x3f000;
-        break;
+        return 0x3f000;
     case CODENAME_MATISSE:
     case CODENAME_VERMEER:
     case CODENAME_LUCIENNE:
     case CODENAME_RENOIR:
     case CODENAME_CEZANNE:
-        psp_dir_addr = 0x4f000;
-        break;
+        return 0x4f000;
     default:
         g_assert_not_reached();
     }
+}
 
-    address_space_write(as, psp_dir_addr, MEMTXATTRS_UNSPECIFIED, psp_dir, psp_dir_size);
+static void copy_psp_dir(AddressSpace *as, zen_rom_infos_t *infos, zen_codename codename)
+{
+    psp_directory_table *psp_dir = infos->psp_dir;
+    size_t psp_dir_size = sizeof(psp_directory_header) +
+                          sizeof(psp_directory_entry) * psp_dir->header.num_entries;
+
+    address_space_write(as, get_psp_dir_addr(codename), MEMTXATTRS_UNSPECIFIED,
+                        psp_dir, psp_dir_size);
+}
+
+static void copy_pubkey(AddressSpace *as, zen_rom_infos_t *infos, zen_codename codename)
+{
+    bool ret;
+    uint32_t key_addr, key_size;
+
+    ret = zen_rom_get_psp_entry(infos, AMD_FW_PSP_PUBKEY, &key_addr, &key_size);
+    assert(ret);
+
+    void *buf = g_malloc(key_size);
+    zen_rom_read(infos, key_addr, buf, key_size);
+    address_space_write(as, get_psp_dir_addr(codename) + 0x410, MEMTXATTRS_UNSPECIFIED, buf, key_size);
 }
 
 static void setup_bootloader(AddressSpace *as, zen_rom_infos_t *infos)
@@ -95,6 +108,7 @@ void psp_on_chip_bootloader(AddressSpace *as, BlockBackend *blk, zen_codename co
     assert(ret);
 
     copy_psp_dir(as, &infos, codename);
+    copy_pubkey(as, &infos, codename);
     setup_bootloader(as, &infos);
     psp_create_config(as, codename);
 }
