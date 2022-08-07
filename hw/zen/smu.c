2b0dd7283c99ab00250f6a5e68ab3d4897ddac0f
Timothée COCAULT <timothee.cocault@gmail.com>
2022-08-07 15:12:23 +0200

    Add mailbox to SMU

--- hw/zen/smu.c
+++ hw/zen/smu.c
@@ -13,20 +13,36 @@ struct SmuState {
     MemoryRegion regs;
 
     uint32_t smu_ok_offset;
+
+    uint32_t mb_cmd_offset;
+    uint32_t mb_status_offset;
+    uint32_t mb_data_offset;
+
+    uint32_t cmd;
+    uint32_t status;
+    uint32_t data[6];
 };
+
+static void smu_execute(SmuState *s)
+{
+    qemu_log_mask(LOG_UNIMP, "%s: execute\n", __func__);
+    s->status = 1;
+}
 
 static uint64_t smu_read(void *opaque, hwaddr addr, unsigned size)
 {
     SmuState *s = SMU(opaque);
 
-    qemu_log_mask(LOG_UNIMP, "%s: unimplemented device read  "
-                  "(offset 0x%lx)\n", __func__, addr);
-
     if(addr == 0x704) {
         return 1;
     } else if(addr == s->smu_ok_offset) {
         return 1;
+    } else if(addr == s->mb_status_offset) {
+        return s->status;
     }
+
+    qemu_log_mask(LOG_UNIMP, "%s: unimplemented device read  "
+                  "(offset 0x%lx)\n", __func__, addr);
 
     return 0;
 }
@@ -34,6 +50,15 @@ static uint64_t smu_read(void *opaque, hwaddr addr, unsigned size)
 static void smu_write(void *opaque, hwaddr addr,
                                 uint64_t value, unsigned size)
 {
+    SmuState *s = SMU(opaque);
+
+    if(addr == s->mb_cmd_offset) {
+        s->cmd = value;
+        smu_execute(s);
+    } else if(addr >= s->mb_data_offset && addr < s->mb_data_offset + 6 * 4) {
+        s->data[(addr - s->mb_data_offset) / 4] = value;
+    }
+
     qemu_log_mask(LOG_UNIMP, "%s: unimplemented device write "
                   "(offset 0x%lx, value 0x%lx)\n",
                   __func__, addr, value);
@@ -82,6 +107,40 @@ static void smu_realize(DeviceState *dev, Error **errp)
     case CODENAME_RENOIR:
     case CODENAME_CEZANNE:
         s->smu_ok_offset = 0x24;
+        break;
+    default:
+        g_assert_not_reached();
+    }
+
+    switch(s->codename) {
+    case CODENAME_SUMMIT_RIDGE:
+    case CODENAME_PINNACLE_RIDGE:
+        /* SmuV9 */
+        s->mb_cmd_offset    = 0x528;
+        s->mb_status_offset = 0x564;
+        s->mb_data_offset   = 0x598;
+        break;
+    case CODENAME_RAVEN_RIDGE:
+    case CODENAME_PICASSO:
+        /* SmuV10 */
+        s->mb_cmd_offset    = 0x528;
+        s->mb_status_offset = 0x564;
+        s->mb_data_offset   = 0x998;
+        break;
+    case CODENAME_MATISSE:
+    case CODENAME_VERMEER:
+        /* SmuV11 */
+        s->mb_cmd_offset    = 0x530;
+        s->mb_status_offset = 0x57c;
+        s->mb_data_offset   = 0x9c4;
+        break;
+    case CODENAME_LUCIENNE:
+    case CODENAME_RENOIR:
+    case CODENAME_CEZANNE:
+        /* SmuV12 */
+        s->mb_cmd_offset    = 0x528;
+        s->mb_status_offset = 0x564;
+        s->mb_data_offset   = 0x998;
         break;
     default:
         g_assert_not_reached();
