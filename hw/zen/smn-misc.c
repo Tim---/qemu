eae55bb1bc3375a8666a3f7772003ee9433a55f5
Timothée COCAULT <timothee.cocault@gmail.com>
2022-08-26 00:13:08 +0200

    Add temp regions for Matisse

--- hw/zen/smn-misc.c
+++ hw/zen/smn-misc.c
@@ -78,14 +78,26 @@ static void add_region(SmnMiscState *s, const char *name, hwaddr addr, uint64_t 
     MemoryRegion *region = g_malloc0(sizeof(*region));
     create_region_with_unimpl(region, OBJECT(s), name, size);
     memory_region_add_subregion(&s->region, addr, region);
-
 }
 
-static void create_dxio(SmnMiscState *s)
+static void add_region_printf(SmnMiscState *s, const char *fmt, hwaddr addr, uint64_t size, ...)
 {
-    if(s->codename != CODENAME_SUMMIT_RIDGE)
-        return;
+    va_list args;
+    va_start(args, size);
 
+    char *name = NULL;
+    int res = vasprintf(&name, fmt, args);
+    assert(res != -1);
+
+    MemoryRegion *region = g_malloc0(sizeof(*region));
+    create_region_with_unimpl(region, OBJECT(s), name, size);
+    memory_region_add_subregion(&s->region, addr, region);
+
+    va_end(args);
+}
+
+static void create_dxio_summit(SmnMiscState *s)
+{
     // These regions are the GMI PCS regions (Physical Coding Sublayer)
     for(int ld = 0; ld < 2; ld++) {
         hwaddr base = 0x12000000 + ld * 0x100000;
@@ -106,7 +118,51 @@ static void create_dxio(SmnMiscState *s)
         g_autofree char *name = g_strdup_printf("XGMI_B%d.PCS", ld);
         add_region(s, name, base + 0x51000, 0x1000);
     }
+}
 
+static void create_dxio_matisse(SmnMiscState *s)
+{
+    struct {
+        uint32_t base;
+        int num_entries;
+    } regions[] = {
+        {.base = 0x12500000, .num_entries = 5},
+        {.base = 0x12900000, .num_entries = 4},
+    };
+
+    for(int i = 0; i < ARRAY_SIZE(regions); i++) {
+        uint32_t base = regions[i].base;
+        int num = regions[i].num_entries;
+
+        for(int j = 0; j < num; j++) {
+            // firmware: 0x1000->0x3000
+            // registers: 0x04008, 0x0401c, 0x04030, 0x04038, 0x04044, 0x04048, 0x04058, 0x0405c, 0x10000, 0x10004, 0x10030
+            add_region_printf(s, "misc_a%d_b%d", base + j * 0x20000, 0x20000, i, j);
+        }
+        for(int j = 0; j < num; j++) {
+            // registers: 0x4fc, 0x838, 0x860, 0x864
+            add_region_printf(s, "misc_a%d_c%d", base + 0xe0000 + j * 0x1000, 0x1000, i, j);
+        }
+
+        // registers: 0x8400, 0x8408, 0x8410, 0x8418, 0x8478
+        add_region_printf(s, "misc_a%d_d", base + 0xf0000, 0x10000, i);
+    }
+
+}
+
+static void create_dxio(SmnMiscState *s)
+{
+    switch(s->codename) {
+    case CODENAME_SUMMIT_RIDGE:
+        create_dxio_summit(s);
+        break;
+    case CODENAME_MATISSE:
+    case CODENAME_VERMEER:
+        create_dxio_matisse(s);
+        break;
+    default:
+        break;
+    }
 }
 
 static void smn_misc_init(Object *obj)
