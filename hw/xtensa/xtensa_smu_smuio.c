0d54efda6afa6bd056217921505690d0607343de
Timothée COCAULT <timothee.cocault@gmail.com>
2022-09-28 13:06:44 +0200

    Add SMUv11

--- hw/xtensa/xtensa_smu_smuio.c
+++ hw/xtensa/xtensa_smu_smuio.c
@@ -51,6 +51,34 @@ static uint64_t smuio_v10_read(void *opaque, hwaddr offset, unsigned size)
     return res;
 }
 
+static uint64_t smuio_v11_read(void *opaque, hwaddr offset, unsigned size)
+{
+    uint64_t res = 0;
+
+    switch(offset) {
+    case 0x884:
+    case 0x888:
+    case 0x88c:
+    case 0x890:
+    case 0x894:
+    case 0x898:
+    case 0x89c:
+    case 0x8a0:
+        res = 0x800000;
+        break;
+    case 0x1130:
+    case 0x1180:
+    case 0x11a8:
+    case 0x1958:
+    case 0x1980:
+        res = 0x10000;
+        break;
+    }
+
+    qemu_log_mask(LOG_UNIMP, "%s: unimplemented device read  (offset 0x%lx, size 0x%x)\n", __func__, offset, size);
+    return res;
+}
+
 static void smuio_write(void *opaque, hwaddr offset,
                             uint64_t data, unsigned size)
 {
@@ -67,6 +95,11 @@ const MemoryRegionOps smuio_v10_ops = {
     .write = smuio_write,
 };
 
+const MemoryRegionOps smuio_v11_ops = {
+    .read = smuio_v11_read,
+    .write = smuio_write,
+};
+
 void create_smuio(MemoryRegion *smn_region, smu_version version)
 {
     const MemoryRegionOps *ops = NULL;
@@ -77,6 +110,9 @@ void create_smuio(MemoryRegion *smn_region, smu_version version)
         break;
     case SMU_V10:
         ops = &smuio_v10_ops;
+        break;
+    case SMU_V11:
+        ops = &smuio_v11_ops;
         break;
     default:
         g_assert_not_reached();
